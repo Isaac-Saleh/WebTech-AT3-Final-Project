664e5769519d521f9dc638ad38bc118b396c0c6b
Isaac-Saleh <118036130+Isaac-Saleh@users.noreply.github.com>
2024-06-08 23:39:14 +0800

    refactor: added all my pages sans the root index to a pages folder and refactored elements of the tailwind.config.js to reach those pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,15 @@
 module.exports = {
   content: [
       './*.html',
+      './pages/*.html'
   ],
   theme: {
     colors: {
       'nav-colour': '#343C4F',
       'bg-dark':'#4A799B',
       'primary-text-dark':'#101A30',
-      'bg-light':'#B6D4DD'
+      'bg-light':'#B6D4DD',
+      'white':'#ffffff'
     },
     extend: {},
   },

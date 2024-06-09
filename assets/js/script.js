e4973e03eaf123360b74f4cd1232222f59343450
Isaac-Saleh <118036130+Isaac-Saleh@users.noreply.github.com>
2024-06-09 17:26:04 +0800

    feat(final proj): Added the aside to desktop that acts as a flyout menu in mobile and has JS functionality for orientation and button click

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+console.log("This is working")
+console.log(screen)
+
+const menu = document.getElementById("flyoutMenu");
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("flyoutButton");
+    button.addEventListener("click", function (event) {
+        event.stopPropagation(); // Prevent click event from bubbling up
+        menu.classList.toggle("hidden");
+    });
+    document.addEventListener("click", function (event) {
+        if ((!menu.contains(event.target)) && screen.availWidth < 820) {
+
+            menu.classList.add("hidden");
+        }
+    })
+});
+
+screen.orientation.addEventListener("change", function(event) {
+    if(screen.availWidth >= 820) {
+        menu.classList.remove("hidden");
+    }
+    if(screen.availWidth < 820){
+        menu.classList.add("hidden");
+    }
+});
